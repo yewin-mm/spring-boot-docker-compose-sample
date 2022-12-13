3b1afecd134ada82bb3c5f772244c100c5cea6b6
Ye Phone Thway <yewin.mmr@gmail.com>
2022-12-13 16:46:16 +0630

    rename class

--- src/main/java/pers/yewin/springbootdocker/SpringBootDockerComposeApplication.java
+++ src/main/java/pers/yewin/springbootdocker/SpringBootDockerComposeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDockerApplication {
+public class SpringBootDockerComposeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootDockerApplication.class, args);
+        SpringApplication.run(SpringBootDockerComposeApplication.class, args);
     }
 
 }
